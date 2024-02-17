b432faaaf18048aaa7d59544a649d397590dda58
felsantoss <felipesantossf@outlook.com>
2024-02-17 12:39:31 -0300

    Resolved - 977.Squares Of a Sorted Array

--- 977-SquaresOfaSortedArray/Program.cs
+++ 977-SquaresOfaSortedArray/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+static int[] SortedSquare(int[] nums)
+{
+    int[] sortedArray = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sortedArray[i] = nums[i] * nums[i];
+    }
+    
+    Array.Sort(sortedArray);
+
+    return sortedArray;
+}
+
+int[] arr = {-7,-3,2,3,11};
+
+Console.WriteLine(String.Join(",", SortedSquare(arr)));
